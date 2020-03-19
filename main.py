0d2ded982b79131baf771256d94149fb2cc18c4c
Mitchell Fenner <mitchfen@protonmail.com>
2020-03-19 19:05:26 -0400

    Added core loop functionality

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import os
+import sys
 import sqlite3
-from addEntry import addEntry
 
+# Add a new tuple to the table
 def addEntry(conn):
     artist = input("Artist: ")
     date = input("Date: ")
     venue = input("Venue: ")
 
-    sql = "INSERT INTO showTABLE (ID, Artist, Date, Venue)\nValues(NULL, '" + artist + "', '" + date + "', '" + venue + "') "
+    sql = "INSERT INTO showTable (ID, Artist, Date, Venue)\nValues(NULL, '" + artist + "', '" + date + "', '" + venue + "') "
 
     conn.execute(sql)
     conn.commit()
 
+# Connect to the SQL Shows.db file
 def connectToDatabase():
     conn = sqlite3.connect('Shows.db')
 
@@ -22,6 +24,24 @@ def connectToDatabase():
     Venue CHAR(30));''')
     return conn
 
+# I miss switch loops
+def mainLoop():
+    keepGoing = True
+    while(keepGoing):
+        print("_____________________")
+        print("1) Print the table")
+        print("2) Add a new entry")
+        print("3) Exit")
+        i = int(input("Command: "))
+        if i == 1: printTable()
+        elif i == 2:addEntry(conn)
+        elif i == 3:keepGoing = False
+
+def printTable():
+    print("table goes here")
+
+    i = input
+
 conn = connectToDatabase()
-addEntry(conn)
+mainLoop() #Executes until user enters 3, then close connection and exit
 conn.close()
