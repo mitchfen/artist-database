55f863d4cb1b7fb5b860344a3645f05c100ade7d
Mitchell Fenner <mitchfen@protonmail.com>
2020-03-27 14:28:43 -0400

    Added basic GUI functionality

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
 
 # Artist Database
 
-Python program using SQLite to build table of artists I've seen.
-For now one table is created which has colums for Artist, Date, Venue.
+I go to a lot of concerts, and was having trouble keeping track of who I have and have not yet seen.
+
+I need a solution that allows me to select all the artists I have seen at a given venue, year, etc. Excel and Excel macros are an OK solution, but  SQLite is better for performing queries. SQL is also more relevant to my future interests. 
+
+This was also a good opportunity to learn how to program a GUI in Python. 
+
+For now, just one table is created which has columns for Artist, Date, Venue.
 
 ## Dependencies
 
-* You'll need sqlite3 on your machine
-* `pip install tabulate`
-* A program to view the db file will be helpful. [sqliteonline](https://sqliteonline.com/) works fine.
+* You will need [sqlite3](https://sqlite.org/download.html) on your machine
+* The tabulate python module: `pip install tabulate`
+* A program to view the db file will be helpful. [sqliteonline](https://sqliteonline.com/) or [SQLiteStudio](https://github.com/pawelsalawa/sqlitestudio/releases)
+
+### Screenshot
+
+![broken-link](https://github.com/mitchfen/artist-database/blob/master/screenshots/screen1.png)
 
 
--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 import os
 import sys
 import sqlite3
+import tkinter as tk
+from tkinter import *
 from sqlite3 import Error
 from tabulate import tabulate
-
-# Add a new tuple to the table
-def addEntry():
-
-    artist = input("Artist: ")
-    date = input("Date: ")
-    venue = input("Venue: ")
-
-    sql = "INSERT INTO showTable (ID, Artist, Date, Venue)\nValues(NULL, '" + artist + "', '" + date + "', '" + venue + "') "
-
-    conn.execute(sql)
-    conn.commit()
 
 # Connect to the SQL Shows.db file and create the only table
 def connectToDatabase():
@@ -31,8 +21,44 @@ def connectToDatabase():
     Venue CHAR(30));''')
     return conn
 
+###################################################
+#   GUI Code
+###################################################
+
+# Add a new tuple to the table
+def addEntryFromGUI():
+    # Casting to string may not be needed here but I'm used to Java/C++, leave it for now
+    print(artistEntry.get())
+    artist = artistEntry.get()
+    date = str(dateEntry.get())
+    venue = str(venueEntry.get())
+
+    sql = "INSERT INTO showTable (ID, Artist, Date, Venue)\nValues(NULL, '" + artist + "', '" + date + "', '" + venue + "') "
+    conn.execute(sql)
+    conn.commit()
+
+def gridAllTheThings():
+    artistLabel.grid(sticky= W, row=0, column=0)
+    artistEntry.grid(row=1, column=0)
+    dateLabel.grid(sticky= W, row=2, column=0)
+    dateEntry.grid(row=3, column=0)
+    venueLabel.grid(sticky= W, row=4, column=0)
+    venueEntry.grid(row=5, column=0)
+    addArtistButton.grid(sticky= W, row=6, column=0)
+
+###################################################
+#   End of GUI code
+###################################################
+
+
+###################################################
+#   Command line code
+#   This code was written for the command line functionality.
+#   No longer needed now that the GUI is functional
+###################################################
+
 # Main program loop, where user input is accepted
-def mainLoop():
+def cmdPromptLoop():
 
     keepGoing = True
     while(keepGoing):
@@ -44,29 +70,66 @@ def mainLoop():
         print("5) Exit")
 
         i = int(input("Command: "))
-        if i == 1:  printTable()
-        elif i == 2: addEntry()
-        elif i == 3: removeEntry()
-        elif i == 4: editEntry()
+        if i == 1:  printTableToCMD()
+        elif i == 2: addEntryFromCMD()
+        elif i == 3: removeEntryFromCMD()
+        elif i == 4: editEntryFromCMD()
         elif i == 5: keepGoing = False
 
 # Print the table using tabulate
-def printTable():
+def printTableToCMD():
         cur.execute("SELECT * FROM showTable")
         entries = cur.fetchall()
         print(tabulate(entries))
 
+def addEntryFromCMD(x):
+    print(x)
+    artist = input("Artist: ")
+    date = input("Date: ")
+    venue = input("Venue: ")
+
+    sql = "INSERT INTO showTable (ID, Artist, Date, Venue)\nValues(NULL, '" + artist + "', '" + date + "', '" + venue + "') "
+
+    conn.execute(sql)
+    conn.commit()
+
+
 # TODO
-def removeEntry():
+def removeEntryFromCMD():
    return 
 
 # TODO
-def editEntry():
+def editEntryFromCMD():
     return
 
+###################################################
+#   End of command line code
+###################################################
+
+# MAIN PROGRAM
 entries = []
 conn = connectToDatabase()
 cur = conn.cursor()
-mainLoop() #Executes until user enters 3, then close connection and exit
-os.system("cp Shows.db backup.db") #copy the database just in case
+
+root = tk.Tk()
+root.geometry("300x200")
+root.resizable(0,0)
+
+root.title("Artist-Database-helper")
+
+artistLabel = Label(root, text="Artist:", anchor="w")
+artistEntry = Entry(root, width=100, borderwidth=0)
+
+dateLabel = Label(root, text="Date:", anchor="w")
+dateEntry = Entry(root, width=100, borderwidth=0)
+
+venueLabel = Label(root, text="Venue:", anchor="w")
+venueEntry = Entry(root, width=100, borderwidth=0)
+
+addArtistButton = Button(root, text="Add", command=lambda: addEntryFromGUI())
+
+gridAllTheThings()
+root.mainloop()
+printTableToCMD()
+
 conn.close()
