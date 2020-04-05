34ef1622609171be72631112c147be225d37a2ed
Mitchell Fenner <mitchfen@protonmail.com>
2020-04-05 08:31:40 -0400

    Updated colorscheme and screenshot

--- src/gui.py
+++ src/gui.py
@@ -9,23 +9,28 @@ def buildGUI(connector, cursor):
 
     # Main Tkinter window
     mainWindow = tk.Tk()
-    mainWindow.geometry("500x500")
+    mainWindow.configure(background = "#44475a")
+    mainWindow.geometry("400x200")
     mainWindow.resizable(0,0)
     mainWindow.title("Artist-Database-helper")
 
     # Labels and text entry boxes
-    artistLabel = Label(mainWindow, text="Artist:")
+    artistLabel = Label(mainWindow, bg="#44475a", fg="#f8f8f2", text="Artist:")
     artistEntry = Entry(mainWindow, width=100, borderwidth=0)
-    dateLabel = Label(mainWindow, text="Date:", )
+    dateLabel = Label(mainWindow, bg="#44475a", fg="#f8f8f2", text="Date:", )
     dateEntry = Entry(mainWindow, width=100, borderwidth=0)
-    venueLabel = Label(mainWindow, text="Venue:")
+    venueLabel = Label(mainWindow, bg="#44475a", fg="#f8f8f2",  text="Venue:")
     venueEntry = Entry(mainWindow, width=100, borderwidth=0)
 
     # Button objects
-    addButton = Button(mainWindow, text="Add", command=lambda: addEntryFromGUI(connector, artistEntry, dateEntry, venueEntry))
-    editButton = Button(mainWindow, text="Edit", command=lambda: editEntryFromGUI())
-    clearButton = Button(mainWindow, text="Clear the table", command=lambda: clearTable(connector, mainWindow))
+    addButton = Button(mainWindow, bg = "#50fa7b", bd = "0", activebackground = "#f1fa8c", text="Add", command=lambda: 
+    addEntryFromGUI(connector, artistEntry, dateEntry, venueEntry))
+    
+    editButton = Button(mainWindow, bg = "#8be9fd", bd = "0", activebackground = "#f1fa8c", text="Edit", command=lambda: editEntryFromGUI())
+    
+    clearButton = Button(mainWindow, bg = "#ff5555", bd = "0", activebackground = "#f1fa8c", text="Clear the table", command=lambda: clearTable(connector, mainWindow))
 
+    # Pace all gui elements with grid method, Sticky is how I am justifying left
     artistLabel.grid(sticky= W, row=0, column=0)
     artistEntry.grid(row=1, column=0)
     dateLabel.grid(sticky= W, row=2, column=0)
@@ -47,6 +52,7 @@ def addEntryFromGUI(connector, artistEntry, dateEntry, venueEntry):
     date = dateEntry.get()
     venue = venueEntry.get()
 
+
     sql = "INSERT INTO showTable (ID, Artist, Date, Venue)\nValues(NULL, '" + artist + "', '" + date + "', '" + venue + "') "
     connector.execute(sql)
     connector.commit()
@@ -56,10 +62,16 @@ def clearTable(connector, mainWindow):
 
     # Bring up a new window to confirm clearing the table
     popupWindow = tk.Toplevel(mainWindow)
-    confirmClear = Button(popupWindow, text="Confirm clear", command=lambda: clearConfirmed(connector, popupWindow))
+    popupWindow.resizable(0,0)
+    #popupWindow.geometry("100x20")
+    popupWindow.title("Confirm")
+    popupWindow.configure(background = "#44475a")
+
+    confirmClear = Button(popupWindow, bg = "#ff5555", bd = "0", activebackground = "#f1fa8c", text="Confirm clear", command=lambda: clearConfirmed(connector, popupWindow))
+    
     confirmClear.grid(sticky = W, row = 0, column = 0)
 
-    # Actually send SQL command to clear table
+    # Actually send SQL command to clear table if confirmation pressed
     def clearConfirmed(connector, popupWindow):
         sql = "DROP TABLE showTable;"
         connector.execute(sql)
