788512304293b1f1d5417ad9fd660a48fc971179
Mitchell Fenner <mitchfen@protonmail.com>
2020-04-05 15:29:16 -0400

    Finished view table - close button in main window

--- src/gui.py
+++ src/gui.py
@@ -12,39 +12,42 @@ def buildGUI(connector, cursor):
     # Main Tkinter window
     mainWindow = tk.Tk()
     mainWindow.configure(background = "#44475a")
-    mainWindow.geometry("400x200")
+    #mainWindow.geometry("400x200")
     mainWindow.resizable(0,0)
     mainWindow.title("Artist-Database-helper")
 
     # Labels and text entry boxes
     artistLabel = Label(mainWindow, bg="#44475a", fg="#f8f8f2", text="Artist:")
-    artistEntry = Entry(mainWindow, width=100, borderwidth=0)
+    artistEntry = Entry(mainWindow, width=30, borderwidth=0)
     
     dateLabel = Label(mainWindow, bg="#44475a", fg="#f8f8f2", text="Date (YYYY-MM-DD):", )
-    dateEntry = Entry(mainWindow, width=100, borderwidth=0)
+    dateEntry = Entry(mainWindow, width=30, borderwidth=0)
     
     venueLabel = Label(mainWindow, bg="#44475a", fg="#f8f8f2",  text="Venue:")
-    venueEntry = Entry(mainWindow, width=100, borderwidth=0)
+    venueEntry = Entry(mainWindow, width=30, borderwidth=0)
 
     # Button objects
     addButton = Button(mainWindow, bg = "#50fa7b", bd = "0", activebackground = "#f1fa8c", text="Add", command=lambda: 
     addEntryFromGUI(connector, artistEntry, dateEntry, venueEntry))
     
-    viewButton = Button(mainWindow, bg = "#8be9fd", bd = "0", activebackground = "#f1fa8c", text="View Table", command=lambda: viewTable(mainWindow))
+    viewButton = Button(mainWindow, bg = "#8be9fd", bd = "0", activebackground = "#f1fa8c", text="View Table", command=lambda: viewTable(mainWindow, cursor))
     
     clearButton = Button(mainWindow, bg = "#ff5555", bd = "0", activebackground = "#f1fa8c", text="Clear the table", command=lambda: clearTable(connector, mainWindow))
+
+    closeButton = Button(mainWindow, bd='0', activebackground = "#f1fa8c", text="Exit", command=lambda: closeMainWindow(mainWindow))
 
     # Pace all gui elements with grid method, Sticky is how I am justifying left
     artistLabel.grid(sticky= W, row=0, column=0)
     artistEntry.grid(row=1, column=0)
-    venueLabel.grid(sticky= W, row=2, column=0)
-    venueEntry.grid(row=3, column=0)
-    dateLabel.grid(sticky= W, row=4, column=0)
-    dateEntry.grid(row=5, column=0)
+    dateLabel.grid(sticky= W, row=2, column=0)
+    dateEntry.grid(row=3, column=0)
+    venueLabel.grid(sticky= W, row=4, column=0)
+    venueEntry.grid(row=5, column=0)
 
     addButton.grid(sticky= W, row=6, column=0)
     viewButton.grid(sticky = W, row=7, column=0)
     clearButton.grid(sticky = W, row=8, column=0)
+    closeButton.grid(sticky=W, row=9, column = 0)
 
     mainWindow.mainloop()
     printTableToCMD(cursor)
@@ -97,22 +100,26 @@ def clearTable(connector, mainWindow):
         popupWindow.destroy()
     
 # TODO
-def viewTable(mainWindow):
+def viewTable(mainWindow, cursor):
     
     # New popup to display the table
     popupWindow = tk.Toplevel(mainWindow)
     popupWindow.resizable(0,0)
-    #popupWindow.geometry("500x500")
     popupWindow.title("Confirm")
     popupWindow.configure(background = "#44475a")
 
+    cursor.execute("SELECT * FROM showTable")
+    entries = []
+    entries = cursor.fetchall()
+    print(entries)
+    
     tempList = [['Test', 'Test'], ['Test2', 'Test2'], ['Test3', 'Test3'], ['Test4', 'Test4']]
 
-    cols = ('Artist', 'Venue', 'Date')
+    cols = ('Number', 'Artist', 'Date', 'Venue')
     listBox = ttk.Treeview(popupWindow, columns=cols, show='headings')
     
-    for i, (Artist, Venue) in enumerate(tempList, start=1):
-        listBox.insert("", "end", values=(i, Artist, Venue))
+    for i, (Number, Artist, Date, Venue) in enumerate(entries, start=1):
+        listBox.insert("", "end", values=(Number, Artist, Date, Venue))
 
     for col in cols:
         listBox.heading(col, text=col)    
@@ -125,3 +132,6 @@ def viewTable(mainWindow):
     closeButton.grid(sticky = W, row=4, column=0)
     printButton = tk.Button(popupWindow, text="Print", width=15, command=close)
     printButton.grid(sticky = E, row=4, column=1)
+
+def closeMainWindow(mainWindow):
+    mainWindow.destroy()
