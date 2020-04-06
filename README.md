c94df128c7930d77867d98c6cc7c9a8b1a6b9294
Mitchell Fenner <mitchfen@protonmail.com>
2020-04-06 00:11:39 -0400

    Simplify back into one file

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 
 # Artist Database Builder
 
-## Overview
+### Overview
 
 I go to a lot of concerts, and was having trouble keeping track of who I have/haven't seen. I need a solution that allows me to select all the artists I have seen at a given venue, year, etc.  
 
 Excel and Excel macros are alright, but  SQLite is better for performing queries.  
 SQLite is also more relevant to my future interests and this project was a good opportunity to learn how to build a GUI in Python.
 
-The program has an alternative command line interface.
+The program has an alternative command line interface and works on Windows 10 and Linux.
 
-## Screens
+### Screens
 
 Main window  
 ![image1 not found](https://github.com/mitchfen/artist-database/blob/master/screenshots/screen1.png)  
@@ -19,22 +19,17 @@ View table screen
 ![image2 not found](https://github.com/mitchfen/artist-database/blob/master/screenshots/screen2.png)
 
 
-## Dependencies
+### Dependencies
 
-Need: 
-
-* You will need [sqlite3](https://sqlite.org/download.html) on your machine.
+* You need [sqlite3](https://sqlite.org/download.html) on your machine.
 * If you are on Linux you may need to install Tkinter manually.
     * `pacman -S python-pmw` on Arch/Manjaro.
     * `apt-get install python-tk` on Debian and its derivatives.
+* Tabulate python module: `pip install tabulate`
+* You may want a program to view the db file but it is not required. I like [sqliteonline.com](https://sqliteonline.com/) and [SQLiteStudio](https://github.com/pawelsalawa/sqlitestudio/releases).
 
-Might want:
+### Running the program
 
-* If you want the commandline version, you'll need the tabulate python module: `pip install tabulate`
-* You may want a program to view the db file. [sqliteonline](https://sqliteonline.com/) or [SQLiteStudio](https://github.com/pawelsalawa/sqlitestudio/releases) both work fine.
+Run the commmand `python main.py`  
 
-## Running the program
-
-`python src/main.py`  
-Release coming soon.
 
--- main.py
+++ main.py
@@ -5,6 +5,94 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import *
 
+from tabulate import tabulate
+
+####################################################
+#
+#       COMMAND LINE CODE
+#
+####################################################
+
+# Main program loop, where user input is accepted
+def cmdPromptLoop(connector, cursor):
+
+    keepGoing = True
+    while(keepGoing):
+
+        print("1) Print the table")
+        print("2) Add a new entry")
+        print("3) Remove an entry")
+        print("4) Edit an entry")
+        print("5) Clear the table")
+        print("6) Exit")
+
+        i = int(input("Command: "))
+        if i == 1:  printTableToCMD(cursor)
+        elif i == 2: addEntryFromCMD(connector)
+        elif i == 3: removeEntryFromCMD(connector)
+        elif i == 4: editEntryFromCMD(connector)
+        elif i == 5: clearTable(connector)
+        elif i == 6: keepGoing = False
+
+# Print the table using tabulate
+def printTableToCMD(cursor):
+
+        cursor.execute("SELECT * FROM showTable")
+        entries = []
+        entries = cursor.fetchall()
+        print(tabulate(entries))
+
+# Get user input from CMD, store in database
+def addEntryFromCMD(connector):
+
+    artist = input("Artist: ")
+    date = input("Date: ")
+    venue = input("Venue: ")
+
+    # TODO: Use regex or some method to validate date format
+
+    sql = "INSERT INTO showTable (ID, Artist, Date, Venue)\nValues(NULL, '" + artist + "', '" + date + "', '" + venue + "') "
+
+    connector.execute(sql)
+    connector.commit()
+
+# Drop and recreate the table to clear it
+def clearTable(connector):
+
+    checkIfSure = input("Are you sure? (yes or no)")
+    keepGoing = True
+    while(keepGoing):
+        if checkIfSure == "yes": 
+            sql = "DROP TABLE showTable;"
+            connector.execute(sql)
+            connector.commit()
+            connector.execute('''CREATE TABLE IF NOT EXISTS showTable(
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            Artist  CHAR(30),
+            Date CHAR(30),
+            Venue CHAR(30));''')
+            connector.commit()
+            keepGoing = False
+        elif checkIfSure == "no":
+            keepGoing = False
+            return
+        else:
+            checkIfSure = input("invalid input, please enter \"yes\" or \"no\"")
+            
+
+# TODO
+def removeEntryFromCMD(connector):
+   return 
+
+# TODO
+def editEntryFromCMD(connector):
+    return
+
+####################################################
+#
+#       GUI CODE
+#
+####################################################
 
 # Create all the GUI elements, and handle button clicks
 def buildGUI(connector, cursor):
@@ -141,3 +229,33 @@ def viewTable(mainWindow, cursor):
 
 def closeMainWindow(mainWindow):
     mainWindow.destroy()
+
+####################################################
+#
+#       CONTROL FLOW STARTS HERE
+#
+####################################################
+
+# Connect to the SQL Shows.db file and create the only table
+try :
+    connector = sqlite3.connect('Shows.db')
+except Error as e:
+    print(e)
+
+connector.execute('''CREATE TABLE IF NOT EXISTS showTable(
+ID INTEGER PRIMARY KEY AUTOINCREMENT,
+Artist  CHAR(30),
+Date CHAR(30),
+Venue CHAR(30));''')
+connector.commit()
+
+cursor = connector.cursor()
+
+# Build the graphical user interface
+buildGUI(connector, cursor)
+
+# Commented out the command line interface now that the GUI is working
+#cmdPromptLoop(connector, cursor)
+
+# Close SQLite connector before exit
+connector.close()
